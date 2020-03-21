86a469b4a964a2df96ca96c8e936ac6a42c8804c
top1Dron <pro100takvoin@gmail.com>
2020-03-21 17:28:06 +0200

    Update script on delete tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.tasks_list, name='list'),
+    path('complete/<int:uid>', views.complete_task, name='complete'),
+    path('delete/<int:uid>', views.delete_task, name='delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tasks.models import TodoItem
 
 
 def index(request):
     return HttpResponse("Simple answer from tasks app")
 
+
+def complete_task(request, uid):
+    task = TodoItem.objects.get(id=uid)
+    task.is_completed = True
+    task.save()
+    return HttpResponse("OK")
+
+
+def delete_task(request, uid):
+    task = TodoItem.objects.get(id=uid)
+    task.delete()
+    return redirect("/tasks/list")
 
 def tasks_list(request):
     all_tasks = TodoItem.objects.all()
