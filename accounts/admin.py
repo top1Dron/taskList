7f590243d75bf224f1b63508251ae7e21e0f57f5
top1Dron <pro100takvoin@gmail.com>
2020-03-26 00:31:16 +0200

    Update base template

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
+from accounts.models import Profile
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'birthdate', 'avatar']
