88d1c1b130f948a434203c41135782be94d85c4c
top1Dron <pro100takvoin@gmail.com>
2020-03-27 17:18:33 +0200

    Try fix

--- taskList/settings.py
+++ taskList/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'false') != "false"
 EMAIL_HOST = os.environ.get('EMAIL_HOST', "smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT"), "587")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'andrew.moshko.send@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'Gm3-Jmw-x64-tJ8')
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'andrew.moshko.send@gmail.com')
